790268a263e3f76f8384f631ae6f577e186838a0
MichaelLourenco <michael.lourenco@pixter.com.br>
2020-10-01 15:42:52 -0300

    The Best Players

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          THE BEST PLAYERS
+          {process.env.REACT_APP_TITLE}
         </p>
       </header>
     </div>
